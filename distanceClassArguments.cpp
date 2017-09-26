772a45fda331731e3135f95b623871c78e4d002e
Garrett <garrettvandyke@mail.weber.edu>
2017-09-26 12:16:47 -0600

    Added add_dist function.

--- distanceClassArguments.cpp
+++ distanceClassArguments.cpp
@@ -14,6 +14,11 @@ private:
     int feet;
     float inches;
 public:
+    Distance(): feet(0), inches(0)  // Constructor with no arguments
+    {/*Empty body*/}
+    Distance(int ft, float in): feet(ft), inches(in)    // Constructor with two arguments
+    {/*Empty body*/}
+
     void setDist(int f, float i)
     {
         feet = f;
@@ -30,6 +35,7 @@ public:
     {
         cout << "Feet " << feet << " and inches " << inches << endl;
     }
+    void add_dist(Distance d1, Distance d2);
 };
 
 // Prototypes
@@ -39,16 +45,39 @@ public:
 int main()
 {
     // Create an instance "instantiate" the class
-    Distance d1, d2;
-
+    Distance d1, d4;
+    Distance d2(8, 2);
+    Distance d3(2, 4);
     // Use methods to set/access data
-    d1.setDist(5, 9.0);
-    d2.getDist();
+    //d1.setDist(5, 9.0);
+    //d2.getDist();
 
     d1.showDist();
     d2.showDist();
+    d3.showDist();
+
+    d4.add_dist(d2, d3);
+    cout << "d4 = ";
+    d4.showDist();
 
     return 0;
 }
 
 // Function Definitions
+// Belongs to the class.  A method for members only
+// Template: type CLASSNAME::Function_name()
+/* 1) Declare prototype inside class
+ * 2) Definition is outside class
+ * 3) Use the scope resolution operator ::
+ */
+void Distance::add_dist(Distance d1, Distance d2)
+{
+    inches = d1.inches + d2.inches;
+    feet = 0;
+    if(inches >= 12.0)
+    {
+        inches -= 12.0;
+        feet++;
+    }
+    feet += d1.feet + d2.feet;
+}
